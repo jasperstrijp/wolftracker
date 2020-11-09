f35a5fdd67a3028561271bb353e2625e60f77e70
Jasper Strijp <jasperstrijp@gmail.com>
2020-11-09 22:29:28 +0100

    chore: initialized project

---
+++ presentation/src/test/java/com/jasperstrijp/wolftracker/api/presentation/PresentationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jasperstrijp.wolftracker.api.presentation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PresentationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
